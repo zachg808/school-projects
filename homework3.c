357cb7762a30116e350756e5de54cd75269508c7
zachg808 <zjgillespie129@gmail.com>
2022-09-14 17:19:34 -1000

    for tracing 'updated'

--- homework3.c
+++ homework3.c
@@ -10,6 +10,11 @@ int main(int argc, char* argv[])
     float score1 = 20.5;
     char ca[3] = "Hi";
 
+    printf("c1 = %X\nc2 = %X\n", c1, c2);
+    printf("ca[0] = %X\nca[1] = %X\nca[2] = %X\n", ca[0], ca[1], ca[2]);
+
+    printf("argc& = %X\nargv& = %X\nnum1& = %X\nnum2& = %X\nc1& = %X\nc2& = %X\nscore1& = %X\nca[0]& = %X\nca[1]& = %X\nca[2]& = %X\n", &argc, &argv, &num1, &num2, &c1, &c2, &score1, &ca[0], &ca[1], &ca[2]);
+
     dummy(num2, c1, ca, score1);
 
     return 0;
@@ -20,6 +25,10 @@ void dummy(int x, char y, char* z, float w)
     x++;
     y++;
     w = w + 2.1;
+    
+    printf("z pointer = %c\nz pointer = %X\n", z, z);
+
+    printf("x& = %X\ny& = %X\nz& = %X\nw& = %X\n", &x, &y, &z, &w);
 
     /* pause here */
 }
