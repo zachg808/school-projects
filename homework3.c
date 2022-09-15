e5bdd45e25c7af179423ea9bf0761bb8f3bf786b
zachg808 <zjgillespie129@gmail.com>
2022-09-14 17:41:47 -1000

    added output

--- homework3.c
+++ homework3.c
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
     float score1 = 20.5;
     char ca[3] = "Hi";
 
-    printf("c1 = %X\nc2 = %X\n", c1, c2);
-    printf("ca[0] = %X\nca[1] = %X\nca[2] = %X\n", ca[0], ca[1], ca[2]);
+    printf("num1 = %X\nnum2 = %X\nc1 = %X\nc2 = %X\n", num1, num2, c1, c2);
+    printf("ca[0] = %c\nca[1] = %c\nca[2] = %c\n", ca[0], ca[1], ca[2]);
 
     printf("argc& = %X\nargv& = %X\nnum1& = %X\nnum2& = %X\nc1& = %X\nc2& = %X\nscore1& = %X\nca[0]& = %X\nca[1]& = %X\nca[2]& = %X\n", &argc, &argv, &num1, &num2, &c1, &c2, &score1, &ca[0], &ca[1], &ca[2]);
 
@@ -26,7 +26,7 @@ void dummy(int x, char y, char* z, float w)
     y++;
     w = w + 2.1;
     
-    printf("z pointer = %c\nz pointer = %X\n", z, z);
+    printf("x = %X\ny = %X\nw = %f\nz pointer = %c\nz pointer = %X\n",x ,y, w, z, z);
 
     printf("x& = %X\ny& = %X\nz& = %X\nw& = %X\n", &x, &y, &z, &w);
 
